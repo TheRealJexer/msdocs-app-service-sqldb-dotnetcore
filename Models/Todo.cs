a486878bf76b8dc42ba51b8e814c25fe09eb260a
TheRealJexer <jason.d.dyck@edu.sait.ca>
2025-04-19 09:49:37 -0500

    test

--- Models/Todo.cs
+++ Models/Todo.cs
@@ -6,7 +6,7 @@ namespace DotNetCoreSqlDb.Models
     public class Todo
     {
         public int ID { get; set; }
-        public string? StudentEmail { get; set; }
+        public string? Description { get; set; }
 
         [DisplayName("Created Date")]
         [DataType(DataType.Date)]
